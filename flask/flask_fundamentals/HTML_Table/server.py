f95d23305289480ab3e6ffc20492f82e92c41e9d
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-09-18 15:58:13 +0300

    staging and update

--- flask/flask_fundamentals/HTML_Table/server.py
+++ flask/flask_fundamentals/HTML_Table/server.py
@@ -5,19 +5,21 @@ from flask import Flask, render_template
 #----------------------------1-halloworld
 
 app = Flask(__name__)    
-#
-@app.route('/table')
-def table():
-    users = [
-    {'first_name' : 'Michael', 'last_name' : 'Choi'},
-    {'first_name' : 'John', 'last_name' : 'Supsupin'},
-    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-    {'first_name' : 'KB', 'last_name' : 'Tonel'}
-            ]
-    return render_template("index.html", x2=users)
 
+@app.route('/')
+def render_list():
+    return render_template("index.html")
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("index.html", student = student_info, length = len(student_info))
 
 if __name__=="__main__":   
     app.run(debug=True)    
-
