095d3c06d9075ad868de544cc2c43bf753e33e34
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-09-19 13:00:19 +0300

    changes on fruits and renew

--- flask/flask_fundamentals/dojo_fruit_store-master/server.py
+++ flask/flask_fundamentals/dojo_fruit_store-master/server.py
@@ -8,7 +8,16 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    print(request.form)
+    straw = request.form['strawberry']
+    rasp = request.form['raspberry']
+    apple = request.form['apple']
+    student_id = request.form['student_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+
+    the_result = str(int(straw)+int(rasp)+int(apple))
+    return render_template("checkout.html",the_result=the_result,straw=straw,rasp=rasp,apple=apple,student_id=student_id,first_name=first_name,last_name=last_name)
 
 @app.route('/fruits')         
 def fruits():
